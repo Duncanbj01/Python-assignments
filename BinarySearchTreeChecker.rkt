cfc7cfbd1f3341830bc7978ee1070bb770218f40
Duncanbj01 <83521298+Duncanbj01@users.noreply.github.com>
2023-11-06 10:29:39 -0600

    Update BinarySearchTreeChecker.rkt
    finished scheme bst checker

--- BinarySearchTreeChecker.rkt
+++ BinarySearchTreeChecker.rkt
@@ -1,16 +1,26 @@
 #lang Scheme
-(define (bst? tree)
-  (let ((value (car tree))
-        (left (cadr tree))
-        (right (caddr tree)))
-    (cond
-      ((null? tree) #t) ; and empty tree is a BST
-      ((and (null? left) (null? right)) #t) ; a leaf node is a BST
-      ((null? left) (and (<= value (car (car right))) (bst? right))) ; only right subtree
-      ((null? right) (and (>= value (car (car left))) (bst? left))) ;only left subtree
-      (else (and (<= value (car (car right))) (>= value (car (car left)))
-                 (bst? left) (bst? right))) ;both subtrees
-      )))
+#| bst? function with list as parameter|#
+(define(bst? ls)
+#|bst-help function with list as parameter, minimum and maximum value assumed|#
+  (bst-help ls -1000000000 1000000000))
+
+(define (bst-help ls minVal maxVal)
+  (cond
+    #|if list is null return true|#
+    ((null? ls) #t)
+    #|if first element is less than minVal return false|#
+    ((< (car ls) minVal) #f)
+    #|if first element is greater than maxVal return false|#
+    ((> (car ls) maxVal) #f)
+    #|else return bst-help for left child and bst-help for right child|#
+    (else
+     (and
+      (bst-help (cadr ls) minVal (- (car ls) 1))
+      (bst-help (caddr ls) (+ (car ls) 1) maxVal)
+      )
+     )
+    )
+  )
 
 (bst? '(4 (2 (1 () ()) (3 () ())) (6 (5 () ()) ()))) ; #t
 (bst? '(4 (2 (1 () ()) (3 () ())) (6 (9 () ()) ()))) ; #f
